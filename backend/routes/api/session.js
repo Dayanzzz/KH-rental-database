ff1e83f91f84f00694d27bda237e667f66316190
AbramBuck <abram.buck@gmail.com>
2024-09-22 00:23:29 -0500

    phase 4 completed

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -51,4 +51,24 @@ router.post(
     }
   );
 
+  router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        //   firstName: user.firstName,
+        //   lastName: user.lastName
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+  );
+
+
   module.exports = router;
