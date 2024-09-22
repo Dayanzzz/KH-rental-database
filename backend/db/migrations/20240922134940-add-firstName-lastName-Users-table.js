6521f1c98997473ded2fe6e43076be1cd8d3d90f
AbramBuck <abram.buck@gmail.com>
2024-09-22 09:05:53 -0500

    updated second migration file

--- backend/db/migrations/20240922134940-add-firstName-lastName-Users-table.js
+++ backend/db/migrations/20240922134940-add-firstName-lastName-Users-table.js
@@ -4,19 +4,19 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users','firstName', {
-        type: Sequelize.STRING(60),  
+    await queryInterface.addColumn('Users', 'firstName', {
+      type: Sequelize.STRING(60),
     }, options);
     await queryInterface.addColumn('Users', 'lastName', {
-      type: Sequelize.STRING(60),  
-    }, options)
+      type: Sequelize.STRING(60),
+    }, options);
   },
 
   async down(queryInterface, Sequelize) {
-    //options.tableName = "Users";
-    await queryInterface.removeColumn('Users','firstName',options.tableName="Users");
-    return queryInterface.removeColumn('Users','lastName',options.tableName="Users");
+    await queryInterface.removeColumn('Users', 'firstName', options);
+    return queryInterface.removeColumn('Users', 'lastName', options);
   }
 };
