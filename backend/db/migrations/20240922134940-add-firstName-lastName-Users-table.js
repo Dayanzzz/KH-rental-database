2869e4b8cad2c9939277e0de52517909d45d091e
AbramBuck <abram.buck@gmail.com>
2024-09-22 09:13:03 -0500

    edited migration file two

--- backend/db/migrations/20240922134940-add-firstName-lastName-Users-table.js
+++ backend/db/migrations/20240922134940-add-firstName-lastName-Users-table.js
@@ -1,6 +1,5 @@
 'use strict';
 
-let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
@@ -9,13 +8,14 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn('Users', 'firstName', {
       type: Sequelize.STRING(60),
-    }, options);
+    }, options.table);
     await queryInterface.addColumn('Users', 'lastName', {
       type: Sequelize.STRING(60),
     }, options);
   },
 
   async down(queryInterface, Sequelize) {
+    options.tableName = "Users"
     await queryInterface.removeColumn('Users', 'firstName', options);
     return queryInterface.removeColumn('Users', 'lastName', options);
   }
