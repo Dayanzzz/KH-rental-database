3d591211598eb8f789be02f2eba65c994cf8fc02
AbramBuck <abram.buck@gmail.com>
2024-09-23 12:09:08 -0500

    seeder data populated - Spots

--- backend/db/seeders/20240923170100-spots-seed-data.js
+++ backend/db/seeders/20240923170100-spots-seed-data.js
@@ -1,25 +1,69 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+const { Spot } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await Spot.bulkCreate([
+      {
+        "address": "123 Disney Lane",
+        "city": "San Francisco",
+        "state": "California",
+        "country": "United States of America",
+        "lat": 37.7645358,
+        "lng": -333.4730327,
+        "name": "App Academy",
+        "description": "Place where web developers are created",
+        "price": 123,
+        "createdAt": "2021-11-19 20:39:36",
+        "updatedAt": "2021-11-19 20:39:36",
+        "avgRating": 3.5,
+        "previewImage": "image url"
+      },
+      {
+        "address": "368 Mickey Lane",
+        "city": "Oakland",
+        "state": "California",
+        "country": "United States of America",
+        "lat": 27.7645358,
+        "lng": -222.4730327,
+        "name": "App Academy",
+        "description": "Place where web developers are created",
+        "price": 123,
+        "createdAt": "2021-11-19 20:39:36",
+        "updatedAt": "2021-11-19 20:39:36",
+        "avgRating": 2.5,
+        "previewImage": "image url"
+      },
+      {
+        "address": "412 Peach Lane",
+        "city": "Nashville",
+        "state": "Tennessee",
+        "country": "United States of America",
+        "lat": 17.7645358,
+        "lng": -111.4730327,
+        "name": "App Academy",
+        "description": "Place where web developers are created",
+        "price": 123,
+        "createdAt": "2021-11-19 20:39:36",
+        "updatedAt": "2021-11-19 20:39:36",
+        "avgRating": 1.5,
+        "previewImage": "image url"
+      }
+    ], { validate: true });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = 'Spots';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      address: { [Op.in]: ['123 Disney Lane', '368 Mickey Lane', '412 Peach Lane'] }
+    }, {});
   }
 };
