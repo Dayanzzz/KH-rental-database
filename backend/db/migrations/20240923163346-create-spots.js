f3162ca5b05000645e2e0e9bb48b7266ad624b8a
AbramBuck <abram.buck@gmail.com>
2024-09-23 16:01:04 -0500

    updated user table with a comment, added original Spot migration up, down options object back

--- backend/db/migrations/20240923163346-create-spots.js
+++ backend/db/migrations/20240923163346-create-spots.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// let options = {};
-// if (process.env.NODE_ENV === 'production') {
-//   options.schema = process.env.SCHEMA;  // define your schema in options object
-// }
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Spots', {
@@ -70,11 +70,9 @@ module.exports = {
       }
     });
   },
+
   async down(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Spots');
+    options.tableName = "Spots";
+    return queryInterface.dropTable(options);
   }
-  // async down(queryInterface, Sequelize) {
-  //   options.tableName = "Spots";
-  //   return queryInterface.dropTable(options);
-  // }
 };
