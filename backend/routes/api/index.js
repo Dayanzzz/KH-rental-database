970398a3a36e8d8f0df6d71e1a35339fe90526a2
Dayanzzz <zakrzewski.dayan@gmail.com>
2024-09-26 10:28:33 -0700

    delete spot image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewRouter = require('./reviews.js');
+const spotImagesRouter = require('./spot-images.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -15,6 +16,7 @@ router.use('/session', sessionRouter);
 router.use('/spots', spotsRouter);
 router.use('/users', usersRouter);
 router.use('/reviews', reviewRouter);
+router.use('/spot-images', spotImagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
