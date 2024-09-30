fa5afbf6bab3a067d9d054ec82244799456a51b3
AbramBuck <abram.buck@gmail.com>
2024-09-30 12:51:16 -0500

    Working on Delete a Reveiw Image

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -17,26 +17,50 @@ const router = express.Router();
 
 router.delete('/:imageId',requireAuth,handleValidationErrors, async (req, res, next) => {
     const { imageId } = req.params;
+    console.log('=======================IN ROUTE HANDLER============================');
+    console.log('===========REQ.USER==================');
+    console.log(req.user);
+    
     const loggedInUserId = req.user.dataValues.id;
-
+    
     try {
       const reviewImageToDelete = await ReviewImage.findByPk(imageId);
-    
-   
+
       if (!reviewImageToDelete) {
         return res.status(404).json({ message: "Review Image couldn't be found" });
       }
-      const reviewOwnerNum = reviewOwnerRecord.userId;
 
-      if (loggedInUserId === reviewOwnerNum) {
-      await reviewImageToDelete.destroy();
-      return res.status(200).json({ message: 'Successfully deleted' });
-      }else {
+      const reviewOfImage = reviewImageToDelete.reviewId;
+      const findReview = await Review.findByPk(reviewOfImage);
+
+      if (!findReview) {
+        return res.status(404).json({ message: "Bad Request"});
+      }
+
+      const reviewOwnerNum = findReview.userId; 
+
+      console.log('========Start==============');
+      console.log('========loggedInUserId==============');
+      console.log(loggedInUserId);
+      console.log('========reviewOwnerNum==============');
+      console.log(reviewOwnerNum);
+      console.log('========reviewImageToDelete==============');
+      console.log(reviewImageToDelete);
+      console.log('========End==============');
+
+      if (loggedInUserId !== reviewOwnerNum) {
         const err = new Error('Forbidden');
         err.status = 403;
         err.errors = { message: 'Body validation error' };
         return next(err);
-    } 
+      } 
+
+      if (loggedInUserId === reviewOwnerNum) {
+      await reviewImageToDelete.destroy();
+      return res.status(200).json({ "message": "Successfully deleted"});
+      }
+      
+
   }catch (error) {
       console.error(error);
       return res.status(500).json({ message: 'Internal server error' });
