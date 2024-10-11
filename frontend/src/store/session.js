8f2a813dd526fa246df104f12f2ecdaf8ace11c3
Dayanzzz <zakrzewski.dayan@gmail.com>
2024-10-11 12:52:00 -0700

    signup

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,6 +30,13 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+  
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
@@ -42,5 +49,26 @@ const sessionReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password
+      })
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+
+
+
 
 export default sessionReducer;
