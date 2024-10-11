b4003b4b89623e3335db1153f365a076ac5ef7d4
Dayanzzz <zakrzewski.dayan@gmail.com>
2024-10-11 15:19:15 -0700

    done

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -15,65 +15,70 @@ function ProfileButton({ user }) {
     const ulRef = useRef();
   
     const toggleMenu = (e) => {
-      e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
-      // if (!showMenu) setShowMenu(true);
-      setShowMenu(!showMenu);
-    };
-  
-    useEffect(() => {
-      if (!showMenu) return;
-  
-      const closeMenu = (e) => {
-        if (ulRef.current && !ulRef.current.contains(e.target)) {
-          setShowMenu(false);
-        }
+        e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+        setShowMenu(!showMenu);
       };
-  
-      document.addEventListener('click', closeMenu);
-  
-      return () => document.removeEventListener('click', closeMenu);
-    }, [showMenu]);
-  
-    const logout = (e) => {
-      e.preventDefault();
-      dispatch(sessionActions.logout());
-    };
-  
-    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
-  
-    return (
+    
+      useEffect(() => {
+        if (!showMenu) return;
+    
+        const closeMenu = (e) => {
+          if (!ulRef.current.contains(e.target)) {
+            setShowMenu(false);
+          }
+        };
+    
+        document.addEventListener('click', closeMenu);
+    
+        return () => document.removeEventListener("click", closeMenu);
+      }, [showMenu]);
+    
+      const closeMenu = () => setShowMenu(false);
+    
+      const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+        closeMenu();
+      };
+    
+      const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+    
+      return (
         <>
-        <button onClick={toggleMenu}>
-          <CiAvocado />
-        </button>
-        <ul className={ulClassName} ref={ulRef}>
-          {user ? (
-            <>
-              <li>{user.username}</li>
-              <li>{user.firstName} {user.lastName}</li>
-              <li>{user.email}</li>
-              <li>
-                <button onClick={logout}>Log Out</button>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <OpenModalButton
-                  buttonText="Log In"
-                  modalComponent={<LoginFormModal />}
-                />
-              </li>
-              <li>
-                <OpenModalButton
-                  buttonText="Sign Up"
-                  modalComponent={<SignupFormModal />}
-                />
-              </li>
-            </>
-          )}
-        </ul>
-      </>
-    );
-  }
-  export default ProfileButton;
+          <button onClick={toggleMenu}>
+            <CiAvocado />
+          </button>
+          <ul className={ulClassName} ref={ulRef}>
+            {user ? (
+              <>
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                  <button onClick={logout}>Log Out</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <OpenModalButton
+                    buttonText="Log In"
+                    onButtonClick={closeMenu}
+                    modalComponent={<LoginFormModal />}
+                  />
+                </li>
+                <li>
+                  <OpenModalButton
+                    buttonText="Sign Up"
+                    onButtonClick={closeMenu}
+                    modalComponent={<SignupFormModal />}
+                  />
+                </li>
+              </>
+            )}
+          </ul>
+        </>
+      );
+    }
+    
+    export default ProfileButton;
